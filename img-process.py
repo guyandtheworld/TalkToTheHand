694dc40cea74a28df4ea20ddff7dc82730d5c14a
Adarsh S <4d4r5h.5@gmail.com>
2017-09-01 14:08:19 +0530

    Add framerate for live feed

--- img-process.py
+++ img-process.py
@@ -1,19 +1,29 @@
 import cv2
 import numpy as np
+import time
+from classifier import Classifier
 
 class HandRecognise(object):
     def __init__(self):
+        self.classifier = Classifier()
+        self.classifier.train()
         self.cap = cv2.VideoCapture(0)
+        self.cap.set(3, 58)
+        self.cap.set(4, 62)
+        self.cap.set(16, 1)
 
     def run(self):
+        count = 0
         while(1):
             _, frame = self.cap.read()
-            grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            print(grayscale)
-            cv2.imshow('gray', grayscale)
-            k = cv2.waitKey(5) & 0xFF
-            if k == 27:
-                break
+            if count%5 == 0:
+                grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imshow('gray', grayscale)
+                k = cv2.waitKey(5) & 0xFF
+                if k == 27:
+                    break
+            print(count)
+            count+=1
 
 hg = HandRecognise()
 hg.run()
